b93776c14e9a20449cc4e8f318007cda30771afa
Abhishek <abhi.harry33@gmail.com>
2023-07-27 17:17:28 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <h2>Lets get start</h2>
       <header className="App-header">
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem title={expenses[0].title} amount={expenses[1].amount} date={expenses[0].date}></ExpenseItem>
         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
       </header>
     </div>
