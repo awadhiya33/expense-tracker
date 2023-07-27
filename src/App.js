8c9ae436cec405f300b8fcf8870389bc115c0d73
abhishek kumar <abhi.harry33@gmail.com>
2023-07-27 16:58:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+
+function App() {
+  const expenses=[
+    {title:'Car Insurence',amount:748.44,date:new Date(2023,2,28)},
+    {title:'Bike Insurence',amount:748.44,date:new Date(2025,2,28)},
+    {title:'motor Insurence',amount:748.44,date:new Date(2021,2,28)},
+    {title:'vehicle Insurence',amount:748.44,date:new Date(2021,2,28)}
+  ]
+  return (
+    <div className="App">
+      <h2>Lets get start</h2>
+      <header className="App-header">
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      </header>
+    </div>
+  );
+}
+
+export default App;
